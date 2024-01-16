d7bd4db72d0c0659a6c27c16cee50bd48fdf9c91
Sasha Alimov <klimdos@gmail.com>
2024-01-16 15:16:12 -0500

    Update delete_s3_versions_and_markers.py

--- delete_s3_versions_and_markers.py
+++ delete_s3_versions_and_markers.py
@@ -15,25 +15,35 @@ def delete_objects(delete_list):
         )
 
 def main():
-    paginator = s3_client.get_paginator('list_object_versions')
+    paginator = s3_client.get_paginator('list_objects_v2')  # Use 'list_objects_v2' instead of 'list_object_versions'
     pages = paginator.paginate(Bucket=BUCKET_NAME)
 
     delete_list = []
     total_deleted = 0
 
     for page in pages:
+        if 'Contents' in page:  # Check for existing objects
+            for obj in page['Contents']:
+                delete_list.append({
+                    'Key': obj['Key'],
+                })
+                if len(delete_list) == 1000:
+                    delete_objects(delete_list)
+                    total_deleted += len(delete_list)
+                    print(f"Total deleted so far: {total_deleted}")
+                    delete_list = []
+
         if 'Versions' in page:
             for version in page['Versions']:
-                if not version['IsLatest']:
-                    delete_list.append({
-                        'Key': version['Key'],
-                        'VersionId': version['VersionId']
-                    })
-                    if len(delete_list) == 1000:
-                        delete_objects(delete_list)
-                        total_deleted += len(delete_list)
-                        print(f"Total deleted so far: {total_deleted}")
-                        delete_list = []
+                delete_list.append({
+                    'Key': version['Key'],
+                    'VersionId': version['VersionId']
+                })
+                if len(delete_list) == 1000:
+                    delete_objects(delete_list)
+                    total_deleted += len(delete_list)
+                    print(f"Total deleted so far: {total_deleted}")
+                    delete_list = []
 
         if 'DeleteMarkers' in page:
             for marker in page['DeleteMarkers']:
